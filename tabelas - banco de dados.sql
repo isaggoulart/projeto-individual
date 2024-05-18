c1df51af1fa8bf16e0419f0f3939bd4394ad3641
isaggoulart <isabela.goulart@sptech.school>
2024-05-18 18:34:28 -0300

    dash com receitas (ainda sem conteúdo, apenas estrutura)

---
+++ tabelas - banco de dados.sql
@@ -0,0 +1,31 @@
+drop database de_barriga_cheia;
+create database de_barriga_cheia;
+use de_barriga_cheia;
+
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(100),
+tipoComida varchar (30),
+dtNasc date, 
+email varchar(45),
+senha varchar(45)
+);
+
+create table tentativa (
+idTentativa int auto_increment,
+fkUsuario int,
+primary key (idTentativa, fkUsuario),
+foreign key (fkUsuario) references usuario (idUsuario),
+inicio datetime,
+fim datetime,
+pontos int
+);
+
+create table mensagem (
+idMensagem int auto_increment,
+fkUsuario int,
+primary key (idMensagem, fkUsuario),
+foreign key (fkUsuario) references usuario (idUsuario),
+titulo varchar(45),
+descricao varchar(250)
+);
