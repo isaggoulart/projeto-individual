c32318b2c3fda6511fa8344d58e33f6d29128208
isaggoulart <isabela.goulart@sptech.school>
2024-05-31 16:14:51 -0300

    detalhes quiz e tela de cadastro

--- tabelas - banco de dados.sql
+++ tabelas - banco de dados.sql
@@ -16,8 +16,6 @@ idTentativa int auto_increment,
 fkUsuario int,
 primary key (idTentativa, fkUsuario),
 foreign key (fkUsuario) references usuario (id),
-inicio datetime default current_timestamp,
-fim datetime default current_timestamp,
 pontos int
 );
 
@@ -36,3 +34,14 @@ select tipoComida
 from usuario group by tipoComida
 order by count(*) desc 
 limit 1;
+
+select * from tentativa;
+
+
+select usuario.nome,
+tentativa.pontos as Pontuação
+from tentativa join usuario on fkUsuario = id
+order by pontos desc
+limit 10;
+
+truncate table tentativa;
